033b715a62169345c05432f65bc8ce1e25f822ad
Rakhshan khan <rakshaankhan1@gmail.com>
2025-12-18 02:02:08 +0530

    feat: send the http only cookie in case of brower and token in case of mobile application request on login route

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ import routes from './routes/index.js'
 import connectDB from './config/db.js'
 import swaggerUi from 'swagger-ui-express'
 import swaggerSpec from './config/swagger.js'
+import cookieParser from 'cookie-parser'
 
 const app = express()
 
@@ -10,8 +11,12 @@ const PORT = process.env.PORT || 5000
 const BASE_URL = process.env.APP_BASE_URL || `http://localhost:${PORT}`
 
 app.use(express.json())
+app.use(cookieParser())
 app.use('/api', routes)
 app.use('/api/docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec))
+app.get('/', (req, res) => {
+  res.send('JobFlow Automator API is running 🚀')
+})
 
 connectDB()
 
